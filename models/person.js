3cc21e674d1c7ada3758f9b11a272e1162c3d013
Matias Isosaari <matias_isosaari@hotmail.com>
2018-02-19 13:15:49 +0200

    lint

--- models/person.js
+++ models/person.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
 const Person = mongoose.model('Person', {
-  name: String,
-  number: String
+    name: String,
+    number: String
 })
 
 module.exports = Person
